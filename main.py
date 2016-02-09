e2b4d484f642907749355b10ad18bff6e58a7a2a
Benoit Bourdon <chinchifou@gmail.com>
2016-02-09 20:06:20 +0100

    Comment code
    - add comments

--- main.py
+++ main.py
@@ -1,29 +1,63 @@
 #! python3
-class Definitions:
 
-	def __init__(self):
+#data structure for languages
+#-------------------------------------------
+french = {
+	'name': 'FR',
+	'genders': ['male', 'female']
+}
+#-------------------------------------------
+
+
+#class where we store "definitions"
+#Definitions = [ {'word': 'toto', 'language': FR}, ... ]
+#-------------------------------------------
+class Dictionary:
+
+	def __init__(self): #constructor
 		self.storage = [] #create instance variable "storage"
 
-	def add_word(self, word, language = "FR"):
+	def add_word(self, word, language = french['name'], gender = 'male'): #add a word to the dicitonary
 		definition = {}
-		definition["word"] = word
-		definition["language"] = language
+		definition['word'] = word
+		definition['language'] = language
+		definition['gender'] = gender
 
 		self.storage.append(definition)
 
+	def find_word(self, word): #find a word in the dictionary
+		answer = []
+		for definition in self.storage:
+			if definition['word'] == word:
+				answer.append(definition)			
+		return answer #return an array of definitions
+#-------------------------------------------
+
+
+#initialization
+#-------------------------------------------
+dictionary = Dictionary() #initialize the "database" by instanciating the "dictionary"
+#-------------------------------------------
+
 
 '''
-#comment to ease testing
+#commented to ease testing
 
 print("Que souhaitez vous faire ?")
 print("1 - Sauvegarder un mot")
 print("2 - Chercher un mot")
 answer = input() #todo use answer
 '''
-dictionary = Definitions() #initialize the "database" by instanciating the "dictionary"
 
-answer = "1"
+#lines for testing
+#-------------------------------------------
+dictionary.add_word("toto") 
+answer = "2"
+#-------------------------------------------
 
+
+#UI
+#-------------------------------------------
 if (answer == "1"):
 	word = input("Entrez un mot svp : ")
 	dictionary.add_word(word)
@@ -31,5 +65,12 @@ if (answer == "1"):
 	print("Etat du dico", dictionary.storage)
 
 else:
-	0
-	#word_to_find = input("Veuillez saisir le mot que vous cherchez :")
+	word_to_find = input("Veuillez saisir le mot que vous cherchez :")
+	words_found = dictionary.find_word(word_to_find)
+
+	print("Nombre de mots trouvés :", len(words_found))
+	print("Mots trouvés :")
+
+	for word in words_found:
+		print(word)
+#-------------------------------------------
