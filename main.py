ff9b7c778e9ec96f05b9bc7a7e86968a9839b5d2
HeleneLandron <helene.landron@telecom-bretagne.eu>
2016-02-09 13:02:12 +0100

    Ajouts des données
    Ajout des données collectées pour la création du mot et de sa traduction
    # Proposition de conception

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 word = input("Entrez un mot svp :")
 print("Le mot rentré est ",word)
+
+language = input("Dans quelle langue souhaitez vous traduire ce mot :")
+print("La langue de traduction est ",language)
+
+translation = input("Entrez sa traduction svp :")
+print("La traduction est ",translation)
+
+
+
+# Solution 1
+# On stocke les mots dans une BDD de type postGreSQL
+
+# Solution 2
+# On écrit les mots dans un fichier txt qu'on lira ensuite
